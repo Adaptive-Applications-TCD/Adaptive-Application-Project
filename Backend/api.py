20dc3118c017cc9a126db6209b9d5e997644e963
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2023-04-20 15:33:23 +0100

    Add files via upload

---
+++ Backend/api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from movie_recommender import *
+
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+#apiData = apiData.to_dict()
+#print(type(apiData))
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/<id>")
+def recommend(id):
+    return content_based_recommender(id).to_dict()
+if __name__ == "__main__":
+    app.run()

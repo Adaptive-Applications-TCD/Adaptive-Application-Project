fadf8895a332fd319eabfff95c0fcbe6f861828e
Daniel Grace <daniel.grace44@hotmail.com>
2023-04-20 18:13:31 +0100

    cleaned up API and auto added imports for python file

--- Backend/api.py
+++ Backend/api.py
@@ -5,19 +5,23 @@ import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
-#apiData = apiData.to_dict()
-#print(type(apiData))
+# apiData = apiData.to_dict()
+# print(type(apiData))
+
 
 @app.route("/")
 def hello():
     return "hello world"
 
-@app.route("/<id>")
+
+@app.route("/recommend/<id>")
 def recommend(id):
     content = content_based_recommender(id)
-    if isinstance(content,str):
+    if isinstance(content, str):
         return content
     else:
         return content.to_dict()
+
+
 if __name__ == "__main__":
     app.run()

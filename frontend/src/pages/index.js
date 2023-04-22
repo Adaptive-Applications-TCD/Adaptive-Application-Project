c203c1a8c92831c9c1049cbf3cd34523c2b649fc
Daniel Grace <daniel.grace44@hotmail.com>
2023-04-22 22:40:10 +0100

    messing around with movie styling

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,20 +1,13 @@
 import { useEffect, useContext, useState } from 'react'
 import { authUserContext } from '@/context/AuthUserContext'
-import { Card, Container, TextField, CardContent, IconButton } from '@mui/material'
+import { Card, Container, TextField, CardContent, IconButton, Typography, CardActions, List, ListItem, ListItemAvatar, Avatar, ListItemText, Grid } from '@mui/material'
+import MovieIcon from '@mui/icons-material/Movie';
 import { Search } from '@mui/icons-material'
 import HomeToolbar from '@/components/toolbar'
-import { contentSearch } from '@/api/search'
-
-// async function search(query) {
-//   let response = await contentSearch(query)
-
-//   return response
-// }
-
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState()
-  const [movies, setMovies] = useState()
+  const [movies, setMovies] = useState([{ title: "killer, the", genres: "Action|Crime|Drama|Thriller" }, { title: "face/off", genres: "Action|Crime|Drama|Thriller" }, { title: "face/off", genres: "Action|Crime|Drama|Thriller" }, { title: "face/off", genres: "Action|Crime|Drama|Thriller" }])
 
 
   const handleSearch = (e) => {
@@ -22,14 +15,16 @@ export default function Home() {
     fetch('/api/recommend/' + searchQuery).then(res => res.json()).then(response => console.log(response))
   }
 
-  useEffect(() => {
-    // fetch('/api/hello').then(res => res.json()).then(response => console.log(response))
-  })
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      <Typography>{movie.title}</Typography>
+    })
+  }
 
   return (
     <>
       <HomeToolbar color="primary" />
-      <Container>
+      <Container fixed>
         <Card sx={{ mt: 5, ml: 30, mr: 30 }} variant='outlined'>
           <CardContent>
             <form onSubmit={handleSearch}>
@@ -50,6 +45,26 @@ export default function Home() {
             </form>
           </CardContent>
         </Card>
+        <Grid>
+          {movies.map((movie, index) =>
+            // <ListItem
+            //   key={index}
+            // >
+            //   <ListItemAvatar>
+            //     <Avatar>
+            //       <MovieIcon />
+            //     </Avatar>
+            //   </ListItemAvatar>
+            //   <ListItemText primary={movie.title} secondary={movie.genres} />
+            // </ListItem>
+            <Card key={index} sx={{ width: 200 }}>
+              <CardContent>
+                <Typography>{movie.title}</Typography>
+              </CardContent>
+            </Card>
+          )}
+        </Grid>
+
       </Container>
     </>
   )
